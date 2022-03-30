8f1ec6e8eb9a7bcce6872cd76c93c887e9e55aab
itzrizvi <shahriar.me.work@gmail.com>
2022-03-30 23:28:13 +0600

    App ready with Express

---
+++ app.js
@@ -0,0 +1,21 @@
+// ALL Requires
+const express = require('express');
+
+
+// PORT
+const PORT = process.env.PORT || 5000;
+// Creating APP
+const app = express();
+
+
+// ROOT
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello From Blog'
+    });
+});
+
+// SERVER LISTEN
+app.listen(PORT, () => {
+    console.log(`Server is Running on PORT ${PORT}`);
+})
