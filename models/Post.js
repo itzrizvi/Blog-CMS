4ca6f7361beea9c46a92a0f64a32c9fbfccb39b0
itzrizvi <shahriar.me.work@gmail.com>
2022-04-01 23:46:24 +0600

    Post Schema Adding

--- models/Post.js
+++ models/Post.js
@@ -10,4 +10,27 @@ const postShema = new Schema({
         trim: true,
         maxlength: 100
     },
+    body: {
+        type: String,
+        required: true
+    },
+    author: {
+        types: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: {
+        type: [String],
+        required: true
+    },
+    thumbnail: String,
+    readTime: String,
+    likes: [Schema.Types.ObjectId],
+    disLikes: [Schema.Types.ObjectId],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 });
