bc76f313ba715c0d1c0d66f47b6a6814b74f6542
itzrizvi <shahriar.me.work@gmail.com>
2022-04-01 23:57:50 +0600

    Comment Schema Added

--- models/Comment.js
+++ models/Comment.js
@@ -14,5 +14,35 @@ const commentSchema = new Schema({
         ref: 'User',
         required: true
     },
+    body: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    replies: [
+        {
+            body: {
+                type: String,
+                required: true
+            },
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                default: new Date()
+            }
+        }
+    ]
 
-});
+}, { timestamps: true });
+
+
+// COMMENT MODEL
+const Comment = model('Comment', commentSchema);
+
+
+// Exports
+module.exports = Comment;
