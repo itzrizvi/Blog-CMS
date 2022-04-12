fe6d3ceef9aaabb22633fef4ad2eda199d4f1b24
itzrizvi <shahriar.me.work@gmail.com>
2022-04-12 13:13:46 +0600

    Sign Up Validator Function Done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,7 @@
 // All Requires
 const router = require('express').Router();
+const { body } = require('express-validator');
+const User = require('../models/User');
 
 // Custom Module/Controller
 const { signUpGetController,
@@ -8,11 +10,44 @@ const { signUpGetController,
     logInPostController,
     logOutController } = require('../controllers/authController');
 
+// Validator Array
+const signupValidator = [
+    body('username')
+        .isLength({ min: 2, max: 15 })
+        .withMessage('Username must be between 2 to 15 chars!!!')
+        .custom(async username => {
+            let userFind = await User.findOne({ username });
+            if (userFind) {
+                return Promise.reject('Username Already in Use!!!')
+            }
+        })
+        .trim(),
+    body('email')
+        .isEmail()
+        .withMessage('Please provide a valid E-mail!!!')
+        .custom(async email => {
+            let emailFind = await User.findOne({ email });
+            if (emailFind) {
+                return Promise.reject('Email is already in use!!!')
+            }
+        })
+        .normalizeEmail(),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Your password must be greater than 6 chars!!!'),
+    body('passwordTwo')
+        .custom((matchPass, { req }) => {
+            if (matchPass !== req.body.password) {
+                return Promise.reject('Password does not matched!!!')
+            }
+        })
+];
+
 
 
 // ROUTES
 router.get('/signUp', signUpGetController);
-router.post('/signUp', signUpPostController);
+router.post('/signUp', signupValidator, signUpPostController);
 
 router.get('/logIn', logInGetController);
 router.post('/logIn', logInPostController);
