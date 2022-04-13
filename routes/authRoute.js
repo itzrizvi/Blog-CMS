483b0abdfea73756572a01a14252fd8fafaefc56
itzrizvi <shahriar.me.work@gmail.com>
2022-04-14 01:47:22 +0600

    Create SignUpValidator a Module with extra file

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,6 @@
 // All Requires
 const router = require('express').Router();
-const { body } = require('express-validator');
-const User = require('../models/User');
+const signupValidator = require('../validator/auth/signupValidator');
 
 // Custom Module/Controller
 const { signUpGetController,
@@ -9,42 +8,6 @@ const { signUpGetController,
     logInGetController,
     logInPostController,
     logOutController } = require('../controllers/authController');
-
-// Validator Array
-const signupValidator = [
-    body('username')
-        .isLength({ min: 2, max: 15 })
-        .withMessage('Username must be between 2 to 15 chars!!!')
-        .custom(async username => {
-            let userFind = await User.findOne({ username });
-            if (userFind) {
-                return Promise.reject('Username Already in Use!!!')
-            }
-        })
-        .trim(),
-    body('email')
-        .isEmail()
-        .withMessage('Please provide a valid E-mail!!!')
-        .custom(async email => {
-            let emailFind = await User.findOne({ email });
-            if (emailFind) {
-                return Promise.reject('Email is already in use!!!')
-            }
-        })
-        .normalizeEmail(),
-    body('password')
-        .isLength({ min: 6 })
-        .withMessage('Your password must be greater than 6 chars!!!'),
-    body('passwordTwo')
-        .isLength({ min: 6 })
-        .withMessage('Password didn\'t matched!!!')
-        .custom((matchPass, { req }) => {
-            if (matchPass !== req.body.password) {
-                return Promise.reject('Password does not matched!!!')
-            }
-        })
-];
-
 
 
 // ROUTES
---
+++ validator/auth/signupValidator.js
@@ -0,0 +1,37 @@
+// ALL REQUIRES
+const { body } = require('express-validator');
+const User = require('../../models/User');
+
+module.exports = [
+    body('username')
+        .isLength({ min: 2, max: 15 })
+        .withMessage('Username must be between 2 to 15 chars!!!')
+        .custom(async username => {
+            let userFind = await User.findOne({ username });
+            if (userFind) {
+                return Promise.reject('Username Already in Use!!!')
+            }
+        })
+        .trim(),
+    body('email')
+        .isEmail()
+        .withMessage('Please provide a valid E-mail!!!')
+        .custom(async email => {
+            let emailFind = await User.findOne({ email });
+            if (emailFind) {
+                return Promise.reject('Email is already in use!!!')
+            }
+        })
+        .normalizeEmail(),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Your password must be greater than 6 chars!!!'),
+    body('passwordTwo')
+        .isLength({ min: 6 })
+        .withMessage('Password didn\'t matched!!!')
+        .custom((matchPass, { req }) => {
+            if (matchPass !== req.body.password) {
+                return Promise.reject('Password does not matched!!!')
+            }
+        })
+];
