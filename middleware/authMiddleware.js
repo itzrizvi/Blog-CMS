68b1bbcb5171746a52215d5a49a1c84787c67293
itzrizvi <shahriar.me.work@gmail.com>
2022-04-16 01:37:56 +0600

    Dashboard Rendered

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,13 +2,13 @@
 const User = require('../models/User');
 // Session Binding User
 exports.bindUserWithReq = () => {
-    return (req, res, next) => {
+    return async (req, res, next) => {
         if (!req.session.isLoggedIn) {
             return next();
         }
 
         try {
-            let user = User.findById(req.session.user._id);
+            let user = await User.findById(req.session.user._id);
             req.user = user;
             next();
         } catch (error) {
