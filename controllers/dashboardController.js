f8429db275e9841266e6e0c584beecab6420eb7a
itzrizvi <shahriar.me.work@gmail.com>
2022-05-01 01:51:00 +0600

    Admin Dashboard Added

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,4 +1,4 @@
 // Dashboard GET Controller
 exports.daboardGetController = (req, res, next) => {
-    res.render('pages/dashboard/dashboard', { title: 'Dashboard' });
+    res.render('pages/dashboard/admin_index.ejs', { title: 'Admin Dashboard' });
 }
